0a925979a1f781df093595a3784e4ed3b7486a17
Thomas <thomas.belge@gmail.com>
2023-02-01 13:37:52 +0100

    yes

--- urls.js
+++ urls.js
@@ -1,7 +1,7 @@
 // Insert the backend URL given by Vercel in this variable
-const BACKEND_URL = '';
+const BACKEND_URL = 'https://mymovizbackend-lovat.vercel.app/';
 
 // Insert the frontend URL given by Vercel in this variable
-const FRONTEND_URL = '';
+const FRONTEND_URL = 'https://mymovizfrontend-lovat.vercel.app/';
 
 module.exports = { BACKEND_URL, FRONTEND_URL }; // Do not edit/remove this line
